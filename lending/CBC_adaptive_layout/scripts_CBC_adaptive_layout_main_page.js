9fc3a43b1b44bfb21ac90a4404d85945246f1cd4
fandenver <glebb11@yandex.ru>
2024-03-02 11:00:42 +0300

    Доработал модальное окно + убрал скрол с body, когда окно открыто

--- lending/CBC_adaptive_layout/scripts_CBC_adaptive_layout_main_page.js
+++ lending/CBC_adaptive_layout/scripts_CBC_adaptive_layout_main_page.js
@@ -1,24 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
     const openModalBtn = document.getElementById('requestCall');
     const openModalBtnTwo = document.getElementById('showOffer');
-    const modal = document.getElementById('modal');
+    const modal = document.getElementById('modalGetCall');
     const closeBtn = document.getElementsByClassName('close')[0];
+
+    function disableScroll() {
+        document.body.classList.add('noScroll');
+    }
+
+    function enableScroll() {
+        document.body.classList.remove('noScroll');
+    }
 
     openModalBtn.addEventListener('click', () => {
         modal.style.display = 'block';
+        disableScroll();
     });
 
     openModalBtnTwo.addEventListener('click', () => {
         modal.style.display = 'block';
+        disableScroll();
     });
 
     closeBtn.addEventListener('click', () => {
         modal.style.display = 'none';
+        enableScroll();
     });
 
     window.addEventListener('click', (event) => {
         if (event.target === modal) {
             modal.style.display = 'none';
+            enableScroll();
         }
     });
 })
