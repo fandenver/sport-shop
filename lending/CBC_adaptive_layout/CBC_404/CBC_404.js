2bb9fd600e87c9f12ff2c145dce8d09c630b8b73
fandenver <glebb11@yandex.ru>
2024-03-03 13:35:55 +0300

    Сделал новую страницу 404, добавил JS, переменные, импорты

---
+++ lending/CBC_adaptive_layout/CBC_404/CBC_404.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('errorBlockBtn').addEventListener('click', function () {
+        window.location.href = 'http://localhost:63342/repos-UI/lending/CBC_adaptive_layout/CBC_Main_Page/CBC_adaptive_layout_main_page.html';
+    });
+});
