e1230fa0ba2724bd63bca70b32d4cc292f67edb2
fandenver <glebb11@yandex.ru>
2024-03-07 20:05:21 +0300

    Добавил переходы картинок в главном баннере

--- lending/CBC_adaptive_layout/CBC_Main_Page/scripts_CBC_adaptive_layout_main_page.js
+++ lending/CBC_adaptive_layout/CBC_Main_Page/scripts_CBC_adaptive_layout_main_page.js
@@ -9,6 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const showProductMenu = document.getElementsByClassName('productMenu')[0];
     const hideSpan = document.getElementsByClassName('mainBanner')[0];
     const hideMainBanner = document.getElementsByClassName('mainBannerImage')[0];
+    const showNewImageRight = document.getElementsByClassName('buttonBannerRight')[0];
+    const showNewImageLeft = document.getElementsByClassName('buttonBannerLeft')[0];
+    const newImage = document.getElementsByClassName('mainBannerImage')[0];
+    const textImage = document.getElementsByClassName('bannerTextOnImage')[0];
+    const circleColorOne = document.querySelector('div.paginationBanner > div:nth-child(1)');
+    const circleColorTwo = document.querySelector('div.paginationBanner > div:nth-child(2)');
+    const circleColorThree = document.querySelector('div.paginationBanner > div:nth-child(3)');
+    const circleColorFour = document.querySelector('div.paginationBanner > div:nth-child(4)');
+    const circleColorFive = document.querySelector('div.paginationBanner > div:nth-child(5)');
+    let currentImageIndex = 0;
 
     function disableScroll() {
         document.body.classList.add('noScroll');
@@ -42,6 +52,62 @@ document.addEventListener('DOMContentLoaded', function () {
     function disableBlock() {
         modal.style.display = 'none';
     }
+
+    function oneEvent(background, backgroundSize) {
+        newImage.style.background = background;
+        newImage.style.backgroundSize = backgroundSize;
+    }
+
+    const imagePaths = [
+        '../images_adaptive_layout/hat/mainBannerImage.png',
+        '../images_adaptive_layout/mapTransition/oneClickImage.png',
+        '../images_adaptive_layout/mapTransition/twoClickImage.png',
+        '../images_adaptive_layout/mapTransition/threeClickImage.png',
+        '../images_adaptive_layout/mapTransition/fourClickImage.png'
+    ];
+
+    const circleArr = [
+        circleColorOne,
+        circleColorTwo,
+        circleColorThree,
+        circleColorFour,
+        circleColorFive
+    ];
+
+    showNewImageRight.addEventListener('click', () => {
+        // Обновляем изображение
+        currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
+
+        oneEvent(`url("${imagePaths[currentImageIndex]}")`, 'cover');
+
+        // Меняем класс у элемента массива circleArr
+        const prevIndex = currentImageIndex === 0 ? imagePaths.length - 1 : currentImageIndex - 1;
+        circleArr[prevIndex].classList.remove('circlePaginationOne');
+        circleArr[currentImageIndex].classList.add('circlePaginationOne');
+
+        if (currentImageIndex === 2) { // Проверка, если текущее изображение - третье в массиве
+            textImage.style.color = 'black'; // Изменение цвета текста
+        } else {
+            textImage.style.color = ''; // Сброс цвета текста
+        }
+    });
+
+    showNewImageLeft.addEventListener('click', () => {
+        circleArr[currentImageIndex].classList.remove('circlePaginationOne');
+
+        // Уменьшаем индекс изображения для перемещения в обратном порядке
+        currentImageIndex = (currentImageIndex === 0) ? imagePaths.length - 1 : currentImageIndex - 1;
+        oneEvent(`url("${imagePaths[currentImageIndex]}")`, 'cover');
+
+        // Добавляем класс предыдущему элементу
+        circleArr[currentImageIndex].classList.add('circlePaginationOne');
+
+        if (currentImageIndex === 2) {
+            textImage.style.color = 'black';
+        } else {
+            textImage.style.color = '';
+        }
+    });
 
     openMenuProducts.forEach(element => {
         element.addEventListener('click', () => {
