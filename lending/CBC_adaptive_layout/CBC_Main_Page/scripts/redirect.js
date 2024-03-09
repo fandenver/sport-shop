49607ffab31d1a2c6113d950c076be88b8c95c2f
fandenver <glebb11@yandex.ru>
2024-03-09 18:59:29 +0300

    Рефакторинг

--- lending/CBC_adaptive_layout/CBC_Main_Page/scripts/redirect.js
+++ lending/CBC_adaptive_layout/CBC_Main_Page/scripts/redirect.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (button.id !== 'requestCall' && button.id !== 'sendRequest') {
             button.addEventListener('click', () => {
                 // window.location.href = '../../CBC_404/CBC_404.html';
-                window.location.href = './CBC_adaptive_layout/CBC_404/CBC_404.html';
+                window.location.href = './CBC_404.html';
             })
         }
     })
