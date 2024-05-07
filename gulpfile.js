34b8585170da7efe86632c40cccd6465b768aa3a
fandenver <glebb11@yandex.ru>
2024-05-07 19:26:02 +0300

    Закончил настройку Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,58 @@
-const gulp = require('gulp');
-const fileinclude = require('gulp-file-include');
+import gulp from "gulp";
 
-gulp.task('fileinclude', function() {
-    return gulp.src('internet-shop/src/*.html')
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(gulp.dest('./internet-shop/src'));
-});
+import {path} from "./gulp/config/path.js";
 
-gulp.task('default', gulp.series('fileinclude'));
+import {plugins} from "./gulp/config/plugins.js";
+
+global.app = {
+    isBuild: process.argv.includes('--build'),
+    isDev: !process.argv.includes('--build'),
+    path: path,
+    gulp: gulp,
+    plugins: plugins
+}
+
+import {copy} from "./gulp/tasks/copy.js";
+import {reset} from "./gulp/tasks/reset.js";
+import {html} from "./gulp/tasks/html.js";
+import {server} from "./gulp/tasks/server.js";
+import {scss} from "./gulp/tasks/scss.js";
+import {js} from "./gulp/tasks/js.js";
+import {images} from "./gulp/tasks/images.js";
+import {otfToTtf, ttfToWoff, fontsStyle} from "./gulp/tasks/fonts.js";
+import {svgSprive} from "./gulp/tasks/svgSpive.js";
+import {zip} from "./gulp/tasks/zip.js";
+import {ftp} from "./gulp/tasks/ftp.js";
+
+function watcher() {
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html); // gulp.series(html, ftp)
+    gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.js, images);
+}
+
+export {svgSprive};
+
+// Последовательная обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+// Основные задачи
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+
+// Построение сценария выполнения задач
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher,
+    // server
+));
+const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
+
+export {dev};
+export {build};
+export {deployZIP};
+export {deployFTP};
+
+gulp.task('default', dev);
+
+// 1:23:52 Gulp 2022
