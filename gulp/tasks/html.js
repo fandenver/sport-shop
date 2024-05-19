0dc7a72011785459dc898ffba53d42349efa64f3
fandenver <glebb11@yandex.ru>
2024-05-19 19:30:41 +0300

    Пофиксил баг с нулевым opacity, доработал gulp для scss

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -12,7 +12,7 @@ export const html = () => {
         ))
         .pipe(fileInclude())
         .pipe(app.plugins.replace(/img\//g, '../../srcInternetShop/img/'))
-        .pipe(app.plugins.replace(/scss\//g, '../../srcInternetShop/scss/'))
+        .pipe(app.plugins.replace(/scss\//g, '../css/'))
         .pipe(app.plugins.replace(/src="scripts\//g, 'src="../../srcInternetShop/scripts/'))
         .pipe(app.plugins.if(
             app.isBuild,
