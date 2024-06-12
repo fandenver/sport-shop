6080f406c11f6768d23644db4be35aeaa47989ff
fandenver <glebb11@yandex.ru>
2024-06-12 13:07:25 +0300

    Убрал лишние файлы из проекта

--- lending/CBC_adaptive_layout/CBC_404/CBC_404.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('errorBlockBtn').addEventListener('click', function () {
-        window.location.href = '../CBC_Main_Page/index.html';
-    });
-});
--- lending/draft/draft.js
+++
@@ -1,7 +0,0 @@
-const numberArrOne = [0, 2, 3, 4];
-
-function draftFun (){
-    return numberArrOne.length
-}
-
-console.log(draftFun())
